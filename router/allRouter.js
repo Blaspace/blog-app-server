85ee947c1c0310659418a259c98f80aed2499c6b
Blaspace <115324109+Blaspace@users.noreply.github.com>
2023-03-09 03:58:47 -0800

    simple change

--- router/allRouter.js
+++ router/allRouter.js
@@ -3,7 +3,7 @@ const login = require('../controler/login')
 const register = require('../controler/register')
 const verify = require('../controler/verify')
 const logout = require('../controler/logout')
-const getUer = require('../controler/getUser')
+const getUser = require('../controler/getUser')
 const createBlog = require('../controler/createBlog')
 const getBlog = require('../controler/getBlog')
 const auth = require('../midlewear/auth')
@@ -24,7 +24,7 @@ router.post('/verify', verify)
 
 router.post('/logout', logout)
 
-router.post('/get',auth, getUer)
+router.post('/get',auth, getUser)
 
 router.post('/newblog', createBlog)
 
