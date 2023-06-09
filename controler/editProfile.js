fba9dc87192d2bc0a508e1b653aa86fde65f5dd9
Blaspace <115324109+Blaspace@users.noreply.github.com>
2023-06-09 07:51:13 -0700

    hello

--- controler/editProfile.js
+++ controler/editProfile.js
@@ -2,7 +2,7 @@ const User = require("../schemas/userSchema");
 
 const handleEditProfile = (req, res) => {
   User.findOneAndUpdate({ _id: req.params.id }, req.body)
-    .then((data) => res.sendStatus(200))
+    .then((data) => res.status(200).send(data))
     .catch((err) => res.sendStatus(400));
 };
 
